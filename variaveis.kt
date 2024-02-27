d63ccda64674f93b766b963260f79279bbff29db
thomasmendonca <rdcharacter10@gmail.com>
2024-02-26 21:47:55 -0300

    criando variáveis

---
+++ variaveis.kt
@@ -0,0 +1,8 @@
+fun main(){
+
+    val nome = "Thomas" //String // pode ser alterada MULTÁVEL
+    var idade = 22 // Int // não pode ser alterada NÃO MULTÁVEL
+
+    println("Nome: $nome, Idade: $idade") // imprimindo o conteúdo da variável
+    println(nome)
+}
